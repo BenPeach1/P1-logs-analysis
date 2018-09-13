21552e07add667bd553103e159f0e450c1692d86
Ben Peach <benjamin.j.peach@gmail.com>
2018-09-13 12:45:26 -0400

    Create initial psql database connection module

--- dbconn.py
+++ dbconn.py
@@ -1,24 +1,13 @@
 # "Database code" for the newsdata logs analysis.
 
-import psycopg2, bleach
+import psycopg2
 
 DBNAME = "news"
 
-
-# def get_posts():
-#   """Return all posts from the 'database', most recent first."""
-#   db = psycopg2.connect(database=DBNAME)
-#   c=db.cursor()
-#   c.execute("Select content, time from posts order by time desc")
-#   posts = c.fetchall()
-#   db.close()
-#   return posts
-
-# def add_post(content):
-#   """Add a post to the 'database' with the current timestamp."""
-#   db = psycopg2.connect(database=DBNAME)
-#   c=db.cursor()
-#   c.execute("insert into posts values (%s)", (content,))  # Better, but ...
-#   #c.execute("insert into posts values (%s)", (bleach.clean(content),))
-#   db.commit()
-#   db.close()
+def get_log_data(strQuery):
+    db = pyscopg2.connect(database=DBNAME)
+    c=db.cursor()
+    c.execute(strQuery)
+    strReturn = c.fetchall()
+    db.close()
+    return strReturn
